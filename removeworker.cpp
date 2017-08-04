75defb398752cf669f7ab9fac3526df0bb4e9932
Masashi Haraki <masa.haraki@gmail.com>
2017-08-04 20:12:08 +0900

    Implement call to "emit OutputConsole" from RemoveWorker.

--- removeworker.cpp
+++ removeworker.cpp
@@ -3,6 +3,7 @@
 #include <QDir>
 #include <QFile>
 #include "removeworker.h"
+#include "mainwindow.h"
 
 namespace Farman
 {
@@ -11,7 +12,10 @@ RemoveWorker::RemoveWorker(const QStringList& paths, QObject *parent)
     : Worker(parent)
     , m_paths(paths)
 {
-
+    connect(this,
+            SIGNAL(outputConsole(const QString)),
+            MainWindow::getInstance(),
+            SLOT(onOutputConsole(const QString)));
 }
 
 RemoveWorker::~RemoveWorker()
@@ -32,6 +36,7 @@ void RemoveWorker::run()
     {
         if(isAborted())
         {
+            emitOutputConsole(tr("Aborted.\n"));
             emitFinished(static_cast<int>(Result::Abort));
 
             return;
@@ -57,29 +62,37 @@ void RemoveWorker::run()
         QFileInfo fileInfo(path);
         if(fileInfo.isDir())
         {
+            emitOutputConsole(QString("%1 ... ").arg(path));
             qDebug() << "remove dir  :" << path;
 
             if(!QDir().rmdir(path))
             {
                 // ディレクトリ削除失敗
+                emitOutputConsole(tr("Failed remove directory.\n"));
                 qDebug() << "remove dir error  :" << path;
                 emitFinished(static_cast<int>(Result::ErrorRemoveDir));
 
                 return;
             }
+
+            emitOutputConsole(tr("Removed directory.\n"));
         }
         else
         {
+            emitOutputConsole(QString("%1 ... ").arg(path));
             qDebug() << "remove file :" << path;
 
             if(!QFile::remove(path))
             {
                 // ファイル削除失敗
+                emitOutputConsole(QString("Failed remove file"));
                 qDebug() << "remove file error :" << path;
                 emitFinished(static_cast<int>(Result::ErrorRemoveFile));
 
                 return;
             }
+
+            emitOutputConsole(tr("Removed file.\n"));
         }
 
         emitProcess(QString(tr("%1 file(s) remove...done.")).arg(progress + 1));
