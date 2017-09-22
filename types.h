4cf170c6d4318207a2c673a4557927ea60f2b3f1
Masashi Haraki <masa.haraki@gmail.com>
2017-09-22 11:44:26 +0900

    defined FIX_{FILTER|SORT}_FLAGS

--- types.h
+++ types.h
@@ -16,8 +16,10 @@ enum class ViewMode : int
     Default = ViewMode::Double,
 };
 
-const QDir::Filters DEFAULT_FILTER_FLAGS = QDir::AllEntries | QDir::AccessMask | QDir::AllDirs | QDir::NoDot;
-const QDir::SortFlags DEFAULT_SORT_FLAGS = QDir::DirsFirst | QDir::IgnoreCase | QDir::Name;
+const QDir::Filters FIX_FILTER_FLAGS = QDir::AllEntries | QDir::AccessMask | QDir::AllDirs | QDir::NoDot;
+const QDir::Filters DEFAULT_FILTER_FLAGS = FIX_FILTER_FLAGS;
+const QDir::SortFlags FIX_SORT_FLAGS = static_cast<QDir::SortFlags>(0);
+const QDir::SortFlags DEFAULT_SORT_FLAGS = FIX_SORT_FLAGS | QDir::DirsFirst | QDir::IgnoreCase | QDir::Name;
 
 // 同名ファイルが存在する場合の処理タイプ
 enum class OverwriteMethodType : int
