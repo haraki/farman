fad28a0441e4b4ffa38d8d1a0dacd352efdf860a
Masashi Haraki <masa.haraki@gmail.com>
2018-01-15 17:41:53 +0900

    implement FileAttributesDialog.

--- fileattributesdialog.cpp
+++ fileattributesdialog.cpp
@@ -1,14 +1,58 @@
-#include "fileattributesdialog.h"
+﻿#include "fileattributesdialog.h"
 #include "ui_fileattributesdialog.h"
 
-FileAttributesDialog::FileAttributesDialog(QWidget *parent) :
+namespace Farman
+{
+
+FileAttributesDialog::FileAttributesDialog(const QString& fileName,
+                                           const QString& userName,
+                                           const QString& groupName,
+                                           QFile::Permissions permissions,
+                                           const QDateTime& created,
+                                           const QDateTime& lastModified,
+                                           QWidget *parent/* = Q_NULLPTR*/) :
     QDialog(parent),
     ui(new Ui::FileAttributesDialog)
 {
     ui->setupUi(this);
+
+    ui->fileNameLabel->setText(fileName);
+
+    ui->ownershipUserLabel->setText(userName);
+    ui->ownershipGroupLabel->setText(groupName);
+
+    // チェックボックスをマウスクリックできないようにするための措置
+    ui->permissionsOwnerReadCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsOwnerWriteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsOwnerExecuteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+
+    ui->permissionsGroupReadCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsGroupWriteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsGroupExecuteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+
+    ui->permissionsOtherReadCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsOtherWriteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+    ui->permissionsOtherExecuteCheckBox->setAttribute(Qt::WA_TransparentForMouseEvents);
+
+    ui->permissionsOwnerReadCheckBox->setChecked((permissions & QFile::Permission::ReadOwner) == QFile::Permission::ReadOwner);
+    ui->permissionsOwnerWriteCheckBox->setChecked((permissions & QFile::Permission::WriteOwner) == QFile::Permission::WriteOwner);
+    ui->permissionsOwnerExecuteCheckBox->setChecked((permissions & QFile::Permission::ExeOwner) == QFile::Permission::ExeOwner);
+
+    ui->permissionsGroupReadCheckBox->setChecked((permissions & QFile::Permission::ReadGroup) == QFile::Permission::ReadGroup);
+    ui->permissionsGroupWriteCheckBox->setChecked((permissions & QFile::Permission::WriteGroup) == QFile::Permission::WriteGroup);
+    ui->permissionsGroupExecuteCheckBox->setChecked((permissions & QFile::Permission::ExeGroup) == QFile::Permission::ExeGroup);
+
+    ui->permissionsOtherReadCheckBox->setChecked((permissions & QFile::Permission::ReadOther) == QFile::Permission::ReadOther);
+    ui->permissionsOtherWriteCheckBox->setChecked((permissions & QFile::Permission::WriteOther) == QFile::Permission::WriteOther);
+    ui->permissionsOtherExecuteCheckBox->setChecked((permissions & QFile::Permission::ExeOther) == QFile::Permission::ExeOther);
+
+    ui->timestampCreatedLabel->setText(created.toString(Qt::DefaultLocaleLongDate));
+    ui->timestampLastModifiedLabel->setText(lastModified.toString(Qt::DefaultLocaleLongDate));
 }
 
 FileAttributesDialog::~FileAttributesDialog()
 {
     delete ui;
 }
+
+}           // namespace Farman
--- fileattributesdialog.h
+++ fileattributesdialog.h
@@ -1,22 +1,35 @@
-#ifndef FILEATTRIBUTESDIALOG_H
+﻿#ifndef FILEATTRIBUTESDIALOG_H
 #define FILEATTRIBUTESDIALOG_H
 
 #include <QDialog>
+#include <QFile>
+#include <QDateTime>
 
 namespace Ui {
 class FileAttributesDialog;
 }
+
+namespace Farman
+{
 
 class FileAttributesDialog : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit FileAttributesDialog(QWidget *parent = 0);
+    explicit FileAttributesDialog(const QString& fileName,
+                                  const QString& userName,
+                                  const QString& groupName,
+                                  QFile::Permissions permissions,
+                                  const QDateTime& created,
+                                  const QDateTime& lastModified,
+                                  QWidget *parent = Q_NULLPTR);
     ~FileAttributesDialog();
 
 private:
     Ui::FileAttributesDialog *ui;
 };
+
+}           // namespace Farman
 
 #endif // FILEATTRIBUTESDIALOG_H
